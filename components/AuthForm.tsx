94e14365a9fe606bae36684843b1890e5c85ca01
PardeshiJyotsna <pardeshijyotsna9@gmail.com>
2024-05-23 12:10:20 +0530

    finalize the app

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import React, {useState} from 'react'
 
 
-
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -20,7 +19,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
-import { authformSchema } from '@/lib/utils';
+import { authformSchema} from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import SignIn from '@/app/(auth)/sign-in/page';
 import SignUp from '@/app/(auth)/sign-up/page';
@@ -47,58 +46,52 @@ const AuthForm = ({type}: {type: string}) => {
       },
     })
    
-    // 2. Define a submit handler.
-    const onSubmit = async (data: z.infer<typeof formSchema>)=> {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      setIsLoading(true);
-      try{
-       //sign up with appwrite  & create plain link token
-       
+        // 2. Define a submit handler.
+        const onSubmit = async (data: z.infer<typeof formSchema>) => {
+          setIsLoading(true);
 
-       if(type === 'sign-up'){
+          try {
+            // Sign up with Appwrite & create plaid token
+            
+            if(type === 'sign-up') {
+              const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+              }
 
-        const userData= {
-          firstName: data.firstName!,
-          lastName: data.lastName!,
-          address1: data.address1!,
-          city: data.city!,
-          state: data.state!,
-          postalCode: data.postalCode!,
-          dateOfBirth: data.dateOfBirth!,
-          ssn:data.ssn!,
-          email:data.email,
-          password:data.password
+              const newUser = await signUp(userData);
 
-         }
-      
-         const newUser = await signUp(userData);
+              setUser(newUser);
+            }
 
-        setUser(newUser);
-       }
+            if(type === 'sign-in') {
+              const response = await signIn({
+                email: data.email,
+                password: data.password,
+              })
 
-       if(type === 'sign-in'){
-        const response = await signIn({
-            email: data.email,
-            password: data.password,
-       })
-
-       if(response) router.push('/')
-       }
-
-      }catch (error) {
-        console.log(error);
-      }finally{
-        setIsLoading(false);
-      }
-    }
-  
-  
+              if(response) router.push('/')
+            }
+          } catch (error) {
+            console.log(error);
+          } finally {
+            setIsLoading(false);
+          }
+        }
 
   return (
     <section className='auth-form'>
       <header className='flex flex-col gap-5 md:gap-8'>
-      <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+        <Link href="/" className="cursor-pointer flex    
+              items-center gap-1 px-4">
             <Image 
               src="/icons/logo.svg"
               width={34}
@@ -106,7 +99,7 @@ const AuthForm = ({type}: {type: string}) => {
               alt="Horizon logo"
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
-          </Link>
+        </Link>
           <div className='flex flex-col gap-1 md:gap-3'>
             <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
               {user 
@@ -125,99 +118,64 @@ const AuthForm = ({type}: {type: string}) => {
           </div>
       </header>
       {user ? (
-        <div className='flex flex-col gap-4'>
-         <PlaidLink user={user} variant="primary"/>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
         </div>
-       ):(
-      <>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            {type === 'sign-up' && (
-              <>
-              <div className='flex gap-4'>
-                <CustomInput
-                  control={form.control} name='firstName' label="First Name"
-                  placeholder="Enter your first name"
-                /> 
-
-                <CustomInput
-                  control={form.control} name='lastName' label="Last Name"
-                  placeholder="Enter your last name"
-                /> 
-              </div>
-
-              <CustomInput
-                control={form.control} name='address1' label="Address"
-                placeholder="Enter your specific Address"
-              /> 
-
-              <CustomInput
-                control={form.control} name='city' label="City"
-                placeholder="Enter your City"
-              /> 
-              <div className='flex gap-4'>
-                <CustomInput
-                  control={form.control} name='state' label="State"
-                  placeholder="Example: NY"
-                /> 
-
-                <CustomInput
-                  control={form.control} name='postalCode' label="Postal Code"
-                  placeholder="Example: 11101"
-                /> 
-              </div>
-
-              <div className='flex gap-4'>
-                <CustomInput
-                  control={form.control} name='dateOfBirth' label="Date of birth"
-                  placeholder="YYYY-MM-DD"
-                /> 
-
-                <CustomInput
-                  control={form.control} name='ssn' label="SSN"
-                  placeholder="Example: 1234"
-                />  
-              </div>
-                </>
-            )}
-          
-          <CustomInput
-            control={form.control} name='email' label="Email"
-            placeholder="Enter your email"
-          />  
-
-          <CustomInput
-            control={form.control} name='password' label="Password"
-            placeholder="Enter your password"
-          /> 
-          <div className='flex flex-col gap-4'>          
-            <Button type="submit" disabled={isLoading} className='form-btn'>
-              {isLoading ? (
+      ): (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === 'sign-up' && (
                 <>
-                <Loader2 size={20} className="animate-spin"/> &nbsp; Loading...
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
+                  </div>
+                  <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
+                  <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
+                    <CustomInput control={form.control} name='ssn' label="SSN" placeholder='Example: 1234' />
+                  </div>
                 </>
-              ): type === 'sign-in' 
-                ? 'Sign-in' : 'Sign-Up'}
-            </Button>
-          </div>
-        </form>
-      </Form>
+              )}
 
-        <footer className='flex justify-center gap-1'>
-            <p className='text-14 font-normal text-gray-600'>{type === 'sign-in'
+              <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
+
+              <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === 'sign-in' 
+                    ? 'Sign In' : 'Sign Up'}
+                </Button>
+              </div>
+            </form>
+          </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === 'sign-in'
               ? "Don't have an account?"
-              :'Already have an account?'
-            }
+              : "Already have an account?"}
             </p>
             <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
               {type === 'sign-in' ? 'Sign up' : 'Sign in'}
             </Link>
-        </footer>
-      </>
-
-      )} 
+          </footer>
+        </>
+      )}
     </section>
   )
-};
+}
 
 export default AuthForm
--- lib/plaid.ts
+++ lib/plaid.ts
@@ -1,12 +1,11 @@
-import { headers } from "next/headers";
-import { Configuration, PlaidApi, PlaidApiAxiosParamCreator, PlaidEnvironments } from "plaid";
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 
 const configuration = new Configuration({
   basePath: PlaidEnvironments.sandbox,
-  baseOptions:{
-    headers:{
-      'PLAID-CLIENT-ID':process.env.PLAID_CLIENT_ID,
-      'PLAID_SECRET': process.env.PLAID_SECRET,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
     }
   }
 })
